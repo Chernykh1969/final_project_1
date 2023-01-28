dc83c8394a83e38a6550d6a3eb34a56c33f870b7
Anatoly <nachan2017@yandex.ru>
2023-01-28 17:23:41 +0300

    Добавил программу

---
+++ program/Program.cs
@@ -0,0 +1,15 @@
+﻿List<string> A = new List<string>(){ "Hello", "2", "world", ":-)" };
+List<string> B = new List<string>();
+
+for (int i = 0; i < A.Count; i++)
+{
+    if (A[i].Length <= 3)
+    {
+        B.Add(A[i]);
+    }
+}
+
+for (int i = 0; i < B.Count; i++)
+{
+    Console.WriteLine(B[i]);
+}
